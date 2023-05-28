0ae566a9e41ba774f3da4a4972fb3fad5eec8610
Elia Partin <arenaselia@gmail.com>
2023-05-27 23:43:42 -0400

    added css

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
 import Deck from "./deck.js";
 import Player from "./player.js";
 
+const eliaDeckElement = document.querySelector(".elia-hand");
+const westDeckElement = document.querySelector(".west-hand");
+const textElement = document.querySelector(".text");
+const TXT_WON = "Won";
+
 function Game() {
   this.player1 = new Player({ name: "Elia" }); // returns an object
   this.player2 = new Player({ name: "West" });
   this.deck = new Deck(); // will have a property of cards
-  this.temCardArray = [];
+  this.tempCardArray = new Player({ name: "tempPlayer" });
 }
-
 
 Game.prototype.deal = function () {
   console.log("deal the cards...: ", this.deck.cards.length);
-  // this.shuffle(this.deck.cards); if you want to shuffle the cards again in the game or the hand
   // Each player starts with 26 cards.
   let j = 0;
   for (let i = 0; i < this.deck.cards.length; i += 2) {
@@ -21,6 +24,7 @@ Game.prototype.deal = function () {
     this.player1.hand.push(this.deck.cards[i]);
     this.player2.hand.push(this.deck.cards[j]);
   }
+  this.updateHandCount();
 };
 
 // "this" is the "game" object
@@ -29,18 +33,9 @@ Game.prototype.deal = function () {
 // game also has a deck property (deck is an object that has a cards property (array))
 // deal the cards by giving each player 1 to 1 instead of splitting the deck in two
 
-Game.prototype.giveWinnerTempCards = function () {
-  let tempSize = 
-  this.tempCardArray;
-  for(let i = 0; i < tempSize; i++) {
-player.hand.push(this.tempCardArray.hand[i]);
-  }
-  this.tempCardArray = [];
-};
-
 Game.prototype.compare = function (value1, value2) {
   // comparing
-  console.log("compare");
+  console.log("compare function va1: " + value1 + " val2: " + value2);
   if (value1 < value2) {
     return -1;
   } else if (value1 === value2) {
@@ -50,79 +45,88 @@ Game.prototype.compare = function (value1, value2) {
   }
 };
 
-
 // Each player flips a card at the same time
 // Find which player has the higher card
 // 1. Player with the highest card takes both cards.
 // 2. Puts them face down on top of his stack
 
-Game.prototype.play = function () {
- console.log('play the cards...: ');
- let player1TotalCards = this.player1.hand.length;
- let player2TotalCards = this.player2.hand.length;
- let totalWinningCards = 52;
- console.log("player 1 hand size: ", player1TotalCards);
- console.log("player 2 hand size: ", player2TotalCards);
- let i = 0;
- while ((player1TotalCards < totalWinningCards) || (player2TotalCards < totalWinningCards)) {
-  let player1TopCard = this.player1.hand.pop();
-  let player2TopCard = this.player2.hand.pop();
+Game.prototype.updateHandCount = function () {
+  eliaDeckElement.innerHTML = this.player1.hand.length;
+  westDeckElement.innerHTML = this.player2.hand.length;
+};
 
-  console.log("Comparing Player 1 card < " + player1TopCard.value + " > to player 2 card < " + player2TopCard.value + " > ");
-  if
-  (this.compare(player1TopCard.value, player2TopCard.value) === 0) {
+Game.prototype.updateRoundWinnerText = function (winnerName, txt) {
+  console.log(winnerName + txt);
+  textElement.innerHTML = winnerName + " " + txt;
+};
+
+Game.prototype.giveWinnerTempCards = function (player) {
+  let tempSize = this.tempCardArray;
+  for (let i = 0; i < tempSize; i++) {
+    // console.log("tempArr size: " + this.tempCardArray.hand.length + "!!!!!!!");
+    player.hand.push(this.tempCardArray.hand[i]);
+  }
+  this.tempCardArray = [];
+};
+
+Game.prototype.play = function (player1, player2) {
+  console.log("play the cards...: ");
+  var player1TotalCards = player1.hand.length;
+  var player2TotalCards = player2.hand.length;
+  let totalWinningCards = 52;
+  console.log("player 1 hand size: ", player1TotalCards);
+  console.log("player 2 hand size: ", player2TotalCards);
+  let i = 0;
+  const player1TopCardOfDeck = player1.hand.shift();
+  const player2TopCardOfDeck = player2.hand.shift();
+  console.log(
+    "Comparing Player 1 card < " +
+      player1TopCardOfDeck.value +
+      " > to player 2 card < " +
+      player2TopCardOfDeck.value +
+      " > "
+  );
+
+  if (
+    this.compare(player1TopCardOfDeck.value, player2TopCardOfDeck.value) === 0
+  ) {
     console.log("Playing War");
-    this.tempCardArray.hand.push(player1TopCard);
-    console.log("cards match temp increased size: ", this.tempCardArray.hand.length)
-  } else if
-  (this.compare(player1TopCard.value, player2TopCard.value) === 1) {
+    this.tempCardArray.hand.push(player1TopCardOfDeck);
+    this.tempCardArray.hand.push(player2TopCardOfDeck);
+    console.log(
+      "cards match temp increased size: ",
+      this.tempCardArray.hand.length
+    );
+    this.updateRoundWinnerText("WAR!", "");
+  } else if (
+    this.compare(player1TopCardOfDeck.value, player2TopCardOfDeck.value) === 1
+  ) {
     console.log("Player 1 card has higher card and takes Player 2 cards.");
-    this.player1.hand.push(player2TopCard);
-    this.player1.hand.push(player1TopCard);
-    this.giveWinnerTempCards(this.player1);
+    this.player1.hand.push(player2TopCardOfDeck);
+    this.player1.hand.push(player1TopCardOfDeck);
+    this.giveWinnerTempCards(player1);
+    this.updateRoundWinnerText(player1.name, TXT_WON);
   } else {
     console.log("player 2 card has higher value so takes player 1 cards.");
-    this.player2.hand.push(player1TopCard);
-    this.player2.hand.push(player2TopCard);
-    this.giveWinnerTempCards(this.player2);
+    this.player2.hand.push(player1TopCardOfDeck);
+    this.player2.hand.push(player2TopCardOfDeck);
+    this.giveWinnerTempCards(player2);
+    this.updateRoundWinnerText(player2.name, TXT_WON);
   }
-  player1TotalCards = this.player1.hand.length;
-  player2TotalCards = this.player2.hand.length;
+  player1TotalCards = player1.hand.length;
+  player2TotalCards = player2.hand.length;
+  this.updateHandCount();
   i++;
+
   console.log("i is now", i);
-  console.log("hand size of player p1: " + player1TotalCards + " P2: " + player2TotalCards);
-  if(i > 10000) {
-    break;
-  }
- }
+  console.log(
+    "hand size of player p1: " + player1TotalCards + " P2: " + player2TotalCards
+  );
+  this.updateHandCount("", "");
+ 
+
+  // if (i > 10000) {
+  //   break;
+  // }
 };
 export default Game;
- 
-
- 
- 
-  
- 
- 
-
-  // if the cards are equal, then initiate war.
-  // during war, player 1 and player 2 - put 3 cards face down, 1 card face up (3+1 & 3+1 = 8 + 2
-  // [the cards that triggered the war]= 10.)
-  // Player  with the higher cards takes both piles (10cards)
-  // if the cards are equal, again, (3+1 & 3+1 = 8 +10 [from previous iteration]= 18).
-
-// deck.prototype.dealCards = function () {};
-// const game = new Game();
-
-// the first time through, let player 1 win if it's war
-// then come back and refactor to handle war situations
-
-
-//   if (this.deck.cards[i].value%2 === 0) { //dividing the length of the array by 2 and checking that the remainder is 0
-//     this.player1.hand.push(this.deck.cards[i]);
-//   }
-//   else {
-//     this.player2.hand.push(this.deck.cards)[i];
-//   }
-
-// user has to click the draw button to play the game and do all the comparisons
--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-import Game from "./game.js"
+import Game from "./game.js";
 
-const game = new Game(); 
+const game = new Game();
 game.deck.shuffle();
+let totalNumOfCards = game.deck.cards.length;
 console.log("main deck", game.deck.cards.length);
 game.deal();
-game.play();
+
+document.querySelector(".playButton").addEventListener("click", () => {
+  if (game.player1.hand.length === totalNumOfCards) {
+    alert(game.player1.name + " Won the Game!");
+  } else if (game.player2.hand.length === totalNumOfCards) {
+    alert(game.player2.name + " Won the Game!");
+  } else {
+    game.play(game.player1, game.player2);
+  }
+});
+// game.play();
 
 // const Deck = function () {}; same for Player and Game and Card (function expression index)
 
+// console.log({ game });
 
-console.log({game})
+// function playCard() {}
 
-function playCard() {
-    
-}
-
-document.querySelector('.player1 > .name').textContent = game.player1.name;
-document.querySelector('.player2 > .name').textContent = game.player2.name;
+// document.querySelector(".player1 > .name").textContent = game.player1.name;
+// document.querySelector(".player2 > .name").textContent = game.player2.name;
 // document.querySelector('.player1 > .hand').textContent = game.player1.hand.length;
-document.querySelector('.player2 > .hand').textContent = game.player2.hand.length;
+// document.querySelector(".player2 > .hand").textContent =
+//   game.player2.hand.length;
 
-document.querySelector('.PlayButton').addEventListener("click", playCard)
-
-
-
+// document.querySelector(".PlayButton").addEventListener("click", playCard);
